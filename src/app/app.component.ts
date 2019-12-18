27fc95cb3bb778420c4bfcd59bb08fc3ebfd844a
MorningFog <veronag@me.com>
2019-12-18 13:47:33 +0000

    Created the forms as per the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('form', { static: false }) submittedForm: NgForm;
+
+  submitted = false;
+
+  subType = 'advanced';
+
+  submittedData = {
+    email: '',
+    subType: '',
+    password: ''
+  };
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.submittedData.email = this.submittedForm.value.userData.useremail;
+    this.submittedData.subType = this.submittedForm.value.userData.subType;
+    this.submittedData.password = this.submittedForm.value.userData.password;
+
+    this.submittedForm.reset(
+      {
+        userData: {
+          subType: 'advanced'
+        }
+      });
+  }
 }
